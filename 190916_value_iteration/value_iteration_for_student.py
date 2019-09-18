d521711d7ee0761151c305057a04a242e1e37112
Yeongcheol Kim <aree91@cnu.ac.kr>
2019-09-18 15:21:14 +0900

    update

--- 190916_value_iteration/value_iteration_for_student.py
+++ 190916_value_iteration/value_iteration_for_student.py
@@ -10,8 +10,7 @@ class Agent:
 
     # get next value function table from the current value function table
     def update_value(self):
-        next_value_table = [[0.0] * self.env.width
-                                    for _ in range(self.env.height)]
+        next_value_table = [[0.0] * self.env.width for _ in range(self.env.height)]
         for state in self.env.get_all_states():
             if state == [2, 2]:
                 next_value_table[state[0]][state[1]] = 0.0
@@ -21,8 +20,12 @@ class Agent:
             # ------------------------- FROM HERE ----------------------------#
             # make value_list s --> s' by certain action a
             #
-            print("Implemement : make value list!!!")
-
+            for possible_action in self.env.possible_actions:
+                next_state = self.env.state_after_action(state, possible_action)
+                pas_value = self.get_value(next_state)
+                pas_value *= self.discount_factor
+                pas_value += self.env.get_reward(state, possible_action)
+                value_list.append(pas_value)
             # ------------------------- UNTIL HERE ---------------------------#
 
             # return the maximum value(it is the optimality equation!!)
@@ -37,19 +40,29 @@ class Agent:
         if state == [2, 2]:
             return []
 
-
         # calculating values for the all actions and
         # append the action to action list which has maximum q value
         # ------------------------- FROM HERE ----------------------------#
         # make action_list ( which is a list for possible next optimal action )
-
-        print("Implemement : make action list!!!")
+        next_actions = [] # substitute action_list.clear()
+        for possible_action in self.env.possible_actions:
+            next_state = self.env.state_after_action(state, possible_action)
+            next_state_value = self.get_value(next_state)
+            next_state_value += self.env.get_reward(state, possible_action)
+            if max_value < next_state_value:
+                next_actions = []
+                max_value = next_state_value
+                next_actions.append(possible_action)
+            elif max_value == next_state_value:
+                next_actions.append(possible_action)
+        [action_list.append(action) for action in next_actions]
 
         # ------------------------- UNTIL HERE ---------------------------#
         return action_list
 
     def get_value(self, state):
         return round(self.value_table[state[0]][state[1]], 2)
+
 
 if __name__ == "__main__":
     env = Env()
